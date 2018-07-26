56cbc753d17e719afc60cb8193ec4112c5ba9673
YangJinSeung <jinsng503@gmail.com>
2018-07-26 23:34:29 +0900

    add create-reactron default source

---
+++ main.js
@@ -0,0 +1,33 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({transparent: true, width: 800, height: 600});
+    
+    mainWindow.loadURL("http://localhost:3000/");
+    mainWindow.setMenu(null);
+    
+    //Show Development Tool
+    //mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
