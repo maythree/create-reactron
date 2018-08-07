bada61afcbaf9b83332e8f697edf9c815d82f7b9
YangJinSeung <jinsng503@gmail.com>
2018-08-08 00:15:34 +0900

    scripts & webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,79 @@
-module.exports = {
-    entry: './src/index.js',
+var webpack = require('webpack');
+var path = require('path');
+var port = 8080;
+var publicPath = 'http://localhost:' + port;
+var spawn = require('child_process').spawn;
 
-    output: {
-        path: __dirname + '/public/',
-        filename: 'bundle.js'
+var buildEntryPoint = function(entryPoint){
+    return [
+        'webpack-dev-server/client?http://localhost:' + port,
+        'webpack/hot/only-dev-server',
+        entryPoint
+    ]
+};
+
+module.exports = {
+    devtool: 'inline-source-map',
+
+    entry: {
+        bundle: buildEntryPoint(path.join(__dirname, 'src/index.js'))
     },
 
-    devtool: '#inline-source-map',
-
-    devServer: {
-        inline: true,
-        port: 3000,
-        contentBase: __dirname + '/public/'
+    output: {
+        path: path.join(__dirname, 'src/dist/'),
+        filename: '[name].js',
+        publicPath,
+        libraryTarget: 'commonjs2'
     },
 
     module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    query: {
-                        cacheDirectory: true,
-                        presets: ['es2015', 'react']
-                    }
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader?',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
                 }
-            ]
+            }
+        ]
+    },
+
+    target: 'electron-renderer',
+
+    node: {
+        __dirname: false
+    },
+
+    resolve: {
+        extensions: ['.js', '.json'],
+        modules: [
+            path.resolve(__dirname + '/src'),
+            'node_modules'
+        ]
+    },
+
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+    ],
+
+    devServer: {
+        port,
+        hot: true,
+        inline: false,
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, 'src/'),
+        publicPath,
+        setup() {
+            spawn('npm', ['run', 'electron-start'], { shell: true, stdio: 'inherit' })
+                .on('close', code => process.exit(code))
+                .on('error', spawnError => console.error(spawnError));
         }
+    }
 };
